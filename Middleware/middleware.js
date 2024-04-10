2d8a127f4b1c920898543ea629553411350f40c2
tshering gurung <tshering@tsherings-air.myfiosgateway.com>
2024-04-10 19:59:18 -0400

    created middleware for checkid, idexist and valid input

---
+++ Middleware/middleware.js
@@ -0,0 +1,57 @@
+const { getRaffle } = require("../queries/raffles");
+
+const validId = (req, res, next) => {
+  const { id } = req.params;
+  if (!Number.isInteger(Number(id)) || Number(id) < 1) {
+    return res
+      .status(400)
+      .json({ error: `id param ${id} must be positive integer;` });
+  } else {
+    next();
+  }
+};
+
+const idExist = async (req, res, next) => {
+  const { id } = req.params;
+  const raffle = await getRaffle(id);
+  if (!raffle) {
+    return res.status(404).json({ error: `id param ${id} doesnot exist;` });
+  } else {
+    next();
+  }
+};
+
+const registar_input_field = ["first_name", "last_name", "email"];
+
+const validInput = (req, res, next) => {
+  const registar = req.body;
+
+  for (let field of registar_input_field) {
+
+        if (typeof registar[field] !== "string") {
+        return res
+            .status(400)
+            .json({
+            error: `${field} field missing or wrong data type, recived ${registar[field]}`,
+            });
+        }
+
+        if (registar[field].length === 0) {
+        return res.status(400).json({ error: `${field} cannot be empty` });
+        }
+    }
+
+   for(let field in registar){
+    if(field !== "phone" && !registar_input_field.includes(field)){
+        return res.status(400).json({ error: `${field} is not allowed. Please fill in the only required fields.`})
+    }
+   } 
+   next()
+};
+
+
+module.exports = {
+    validId,
+    idExist,
+    validInput
+}
